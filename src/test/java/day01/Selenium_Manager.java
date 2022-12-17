e8d3c69659f674338799b04736c23dafb907dbf9
ebrarilhan <salihaebrarilhan@gmail.com>
2022-12-17 22:33:36 +0100

    17-12-22

---
+++ src/test/java/day01/Selenium_Manager.java
@@ -0,0 +1,22 @@
+package day01;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.manager.SeleniumManager;
+
+public class Selenium_Manager {
+
+    public static void main(String[] args) {
+
+        SeleniumManager.getInstance().getDriverPath("chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.close();
+        driver.quit();
+
+        SeleniumManager.getInstance().getDriverPath("IEDriverServer");
+        InternetExplorerDriver driver2 = new InternetExplorerDriver();
+        driver2.get("https://www.amazon.com");
+
+    }
+}
---
+++ src/test/java/day01_DriverMethods/Day01_NavigateMethod.java
@@ -0,0 +1,36 @@
+package day01_DriverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day01_NavigateMethod {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        //Amazon sayfasina gidelim.
+        driver.navigate().to("https://amazon.com");
+
+        //techpro sayfasına gidelim
+        Thread.sleep(3000);
+        driver.navigate().to("https://techproeducation.com"); //driver.get ile de ayni sonuclari aliriz ancak drivergette sayfanin yuklenmesini
+        //bekliyor navigate oldugunda beklemiyor.
+
+        //Tekrar amazon sayfasına dönelim
+        Thread.sleep(3000);
+        driver.navigate().back(); //amazon sayfasina geri doner
+
+        //Tekrar techpro sayfasına gidelim.
+        Thread.sleep(3000);
+        driver.navigate().forward(); //techproya gider.
+
+        //Son sayfada sayfayı yenileyelim(refresh)
+        Thread.sleep(3000);
+        driver.navigate().refresh();
+
+        //Ve sayfayı kapatalım
+        driver.close();
+
+    }
+}
